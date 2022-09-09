fb53a097faaf74aaaeed5676ebe27c2fa3a619fb
therecluse26 <therecluse26@protonmail.com>
2022-09-09 13:59:07 -0400

    Added ziggy and next.js

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Tightenco\Ziggy\Ziggy;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +16,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+Route::get('/ziggy', fn () => response()->json(new Ziggy));
+
+Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
+
