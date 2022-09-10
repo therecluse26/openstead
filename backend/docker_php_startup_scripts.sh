b5950bdc673b99944b5f18523e64f6a06add724c
therecluse26 <therecluse26@protonmail.com>
2022-09-09 22:52:48 -0400

    Fixing artisan reference issue in startup

--- backend/docker_php_startup_scripts.sh
+++ backend/docker_php_startup_scripts.sh
@@ -2,17 +2,17 @@
 
 # Runs non-worker startup scripts (migrations, primarily)
 if [ "$APP_ENV" != "worker" ]; then
-php artisan package:discover
-php artisan migrate --force
-php artisan optimize
-php artisan route:cache
-php artisan queue:restart
+/usr/local/bin/php /var/www/html/artisan package:discover
+/usr/local/bin/php /var/www/html/artisan migrate --force
+/usr/local/bin/php /var/www/html/artisan optimize
+/usr/local/bin/php /var/www/html/artisan route:cache
+/usr/local/bin/php /var/www/html/artisan queue:restart
 fi
 
 # Excludes migrations from worker startup
 if [ "$APP_ENV" = "worker" ]; then
-php artisan package:discover
-php artisan optimize
-php artisan route:cache
-php artisan queue:restart
+/usr/local/bin/php /var/www/html/artisan package:discover
+/usr/local/bin/php /var/www/html/artisan optimize
+/usr/local/bin/php /var/www/html/artisan route:cache
+/usr/local/bin/php /var/www/html/artisan queue:restart
 fi
