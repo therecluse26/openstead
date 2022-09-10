4b6faa7de08fabcaefbf2f8a0cea3aec358c1ec3
therecluse26 <therecluse26@protonmail.com>
2022-09-09 22:58:44 -0400

    Fixing startup issues

--- backend/docker_startup.sh
+++ backend/docker_startup.sh
@@ -1,3 +1,9 @@
 #!/bin/ash
 
+/usr/local/bin/php /var/www/html/artisan package:discover
+/usr/local/bin/php /var/www/html/artisan migrate --force
+/usr/local/bin/php /var/www/html/artisan optimize
+/usr/local/bin/php /var/www/html/artisan route:cache
+/usr/local/bin/php /var/www/html/artisan queue:restart
+
 /usr/bin/supervisord -n -c /etc/supervisord.conf
