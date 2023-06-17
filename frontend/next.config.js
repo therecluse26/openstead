cf5346150173b6ab715863a3c6ef90b8902d5cf2
therecluse26 <therecluse26@protonmail.com>
2023-06-17 00:04:22 -0400

    Trying flask style routing

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -4,4 +4,15 @@ module.exports = {
     images: {
         domains: ['minio', 'openstead.app'],
     },
+    rewrites: async () => {
+        return [
+            {
+                source: '/api/:path*',
+                destination:
+                    process.env.NODE_ENV === 'development'
+                        ? 'http://127.0.0.1:8000/api/:path*'
+                        : '/api/',
+            },
+        ]
+    },
 }
