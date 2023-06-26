1d4afd9fda9ecabb7205349839e45696a9bff339
therecluse26 <therecluse26@protonmail.com>
2023-06-26 12:38:58 -0400

    Re-added nginx, I think the config should be good now (hopefully)

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -4,6 +4,9 @@ const axios = Axios.create({
     baseURL: process.env.NEXT_PUBLIC_BACKEND_URL,
     headers: {
         'X-Requested-With': 'XMLHttpRequest',
+        'Access-Control-Max-Age': 'public, max-age=7200',
+        'Access-Control-Allow-Origin': process.env.NEXT_PUBLIC_BACKEND_URL,
+        Vary: 'origin',
     },
     withCredentials: true,
 })
