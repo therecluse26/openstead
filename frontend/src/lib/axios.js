968aa6b676edf5bb32b3bfc03c005d88dcfa6eda
therecluse26 <therecluse26@protonmail.com>
2023-08-13 21:23:48 -0400

    Replaced axios baseurl environment variable with hardcoded url

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,11 +1,11 @@
 import Axios from 'axios'
 
 const axios = Axios.create({
-    baseURL: process.env.NEXT_PUBLIC_BACKEND_URL,
+    baseURL: 'https://api.openstead.app', // I can't figure out how to get the backend URL from the environment variable when running statically
     headers: {
         'X-Requested-With': 'XMLHttpRequest',
         'Access-Control-Max-Age': 'public, max-age=7200',
-        'Access-Control-Allow-Origin': process.env.NEXT_PUBLIC_BACKEND_URL,
+        'Access-Control-Allow-Origin': 'https://api.openstead.app',
         Vary: 'origin',
     },
     withCredentials: true,
